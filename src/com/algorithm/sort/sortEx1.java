f0791004fd49feabdf229d0f45ad7c9761cfd9ef
yoonhyunjeong <yoonhyunjeong@218.38.137.27>
2021-10-04 23:47:02 +0900

    sort commit

--- src/com/algorithm/sort/sortEx1.java
+++ src/com/algorithm/sort/sortEx1.java
@@ -13,23 +13,18 @@ public class sortEx1 {
 		/**
 		 * 정렬이란 ? 데이터를 특정한 기준에 따라 순서대로 나열하는것 
 		 *
-		 *	1. 선택정렬 알고리즘 : 처리되지 않은 데이터중 가장 작은 데이터를 선택하여 맨앞에 있는 데이터와 바꾸는 것 !
+		 *	1. Swap 알고리즘 : 
 		 */
+		 int[] arr = {3, 5};
 
-		int[] arr = {7,2,3,4,2};
-		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i+1; j < arr.length; j++) {
-				int tmp = i;
-				if(arr[i] > arr[j]) {
-					tmp = j;
-				}
-			}
-			
-			int value = arr[i];
-			arr[i] = 
-			
-		}
+	        // 스와프(Swap)
+	        int temp = arr[0];
+	        System.out.println(arr[0] +" ,"+ arr[1]);
+	        arr[0] = arr[1];
+	        System.out.println(arr[0] +" ,"+ arr[1]);
+	        arr[1] = temp;
+
+	        System.out.println(arr[0] + " " + arr[1]);
 		
 	}
 
