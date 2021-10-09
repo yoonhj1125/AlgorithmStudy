fc33d6932e68d67c1d538b5c844b2bc225ba20bd
yoonhyunjeong <yoonhyunjeong@218.38.137.27>
2021-10-09 23:20:05 +0900

    commit

--- src/com/algorithm/sort/sortEx9.java
+++ src/com/algorithm/sort/sortEx9.java
@@ -4,84 +4,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-class Student implements Comparable<Student>{
-	private String name;
-	private int kor;
-	private int eng;
-	private int math;
-	
-	public void Student(String name, int kor, int eng, int math) {
-		this.name = name;
-		this.kor = kor;
-		this.eng = eng;
-		this.math = math;
-		
-	}	
-	
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	//점수가 낮은순서 
-	public int compareTo(Student o) {
-		if(this.kor == o.kor && this.eng == o.eng && this.math == o.math) {
-			return this.name.compareTo(o.name);
-		}
-		if(this.kor == o.kor && this.eng == o.eng) {
-			return Integer.compare(o.math, this.math);
-			
-		}
-		if(this.kor == o.kor) {
-			return Integer.compare(this.eng, o.eng);
-		}
-		return Integer.compare(o.kor, this.kor);
-	}
-	
-}
-
-
 public class sortEx9 {
-	
-		
+	 public static final int MAX_VALUE = 9;
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//8. 정렬 알고리즘 : 
+		//9. 정렬 알고리즘 : 
 		
 		/**
 		 * 정렬이란 ? 데이터를 특정한 기준에 따라 순서대로 나열하는것 
 		 *
-		 *	8. 국영수 문제
-		 *
-		 *	도현이네 반 학생 N명의 이름과 국어, 영어, 수학 점수가 주어진다. 
-		 *
-		 *	1. 국어점수가 감소하는 순서로
-		 *	2. 국어 점수가 같으면 영어점수가 증가하는 순서로
-		 *	3. 국어점수와 영어점수가 같으면 수학점수가 감소하는 순서로
-		 *	4, 모든점수가 같으면 이름이 사전순으로 증가하는 순서로(단, 아스키에서 대문자는 소문자보다 작으므로 사전순으로 앞에 온다)
-		 *
-		 *
+		 *	9. 안테나 문제
+		 *	일진선 상의 마을에 여러채의 집이 위치해 있다. 이중에서 특정 위치의 집에 특히 한개의 안테나를 설치하기로 했다. 효율성을 위해 안테나로부터 모든 집까지의 거리의 총 합이 최소가 되도록 하려고 한다.
+		 *	논리적으로 동일한 위치에 여러 채의 집이 존재하는 것은 불가능하다.
+		 *	집들의 위치 값이 주어질 때, 안테나를 설치할 위치를 선택하는 프로그램을 작성해라.
+		 *	예를 들어 N = 4 인경우,각 위치는 1,5,7,9라고 가정하자. 이 경우, 5의 위치에 설치했을 떄, 안테나로부터 모든 집까지의 거리의 총 합이 4+0+2+4 = 10 으로 최소가 된다.
 		 */
 		
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();	//학생수
+		int n = sc.nextInt();
 		
-		ArrayList<Student> array = new ArrayList<Student>();
+		ArrayList<Integer> array = new ArrayList<Integer>();
 		for (int i = 0; i < n; i++) {
-			String name = sc.next();
-			int kor = sc.nextInt();
-			int eng = sc.nextInt();
-			int math = sc.nextInt();
-			
+			array.add(sc.nextInt());
 		}
-
+		System.out.println(array);
+		
 		Collections.sort(array);
-		 
-		for (int i = 0; i < n; i++) {
-			System.out.println(array.get(i).getName());
-		}
 		
+		System.out.println(array.get(n-1)/2);
 	}
 
 }
