dd8b42fc847a43d8e7176473cbdd839baad37176
yoonhyunjeong <yoonhyunjeong@localhost>
2021-10-16 18:42:18 +0900

    다이나

--- src/com/algorithm/dynamic/dynamicProgrammingEx1.java
+++ src/com/algorithm/dynamic/dynamicProgrammingEx1.java
@@ -2,7 +2,31 @@ package com.algorithm.dynamic;
 
 import java.util.Scanner;
 
+
 public class dynamicProgrammingEx1 {
+
+    // 피보나치 함수(Fibonacci Function)을 재귀함수로 구현
+    public static int fibo(int x) {
+        if (x == 1 || x == 2) {
+            return 1;
+        }
+        return fibo(x - 1) + fibo(x - 2);
+    }
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		result = fibo(n);
+		System.out.println(result);
 	
 	
+		int[] arr = {3,4,5};
+		int lentgh = arr.length;
+		System.out.println(lentgh);
+	}	
 }
+	
+	
+
