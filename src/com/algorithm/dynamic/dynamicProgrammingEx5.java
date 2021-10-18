a631d3ddfd046c82bce8c968047d88ecd879e665
yoonhyunjeong <yoonhyunjeong@218.38.137.27>
2021-10-18 23:23:32 +0900

    commit

--- src/com/algorithm/dynamic/dynamicProgrammingEx5.java
+++ src/com/algorithm/dynamic/dynamicProgrammingEx5.java
@@ -11,38 +11,28 @@ public class dynamicProgrammingEx5 {
 	/**
 	 * 
 	 */
-	public static int solution(int x) {
+    // 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화 
+    public static int[] d = new int[100];
 
-        // 다이나믹 프로그래밍(Dynamic Programming) 진행(보텀업)
-        for (int i = 2; i <= x; i++) {
-            // 현재의 수에서 1을 빼는 경우
-        	System.out.println("i : "+ i);
-           d[i] = d[i - 1] + 1;
-           System.out.println(d[i]);
-            // 현재의 수가 2로 나누어 떨어지는 경우
-            if (i % 2 == 0) {
-            	d[i] = Math.min(d[i], d[i / 2] + 1);
-            	System.out.println("여기?1 : " + d[i]);
-            }
-            // 현재의 수가 3으로 나누어 떨어지는 경우
-            if (i % 3 == 0) {
-            	d[i] = Math.min(d[i], d[i / 3] + 1);
-            	System.out.println("여기?2 : "+ d[i]);
-            }
-            // 현재의 수가 5로 나누어 떨어지는 경우
-            if (i % 5 == 0) {
-            	d[i] = Math.min(d[i], d[i / 5] + 1);
-            	System.out.println("여기?3 : "+ d[i]);
-            }
-        }
-		return d[x];
+	public static void solution(int n, int arr[]) {
+		d[0] = arr[0];
+		d[1] = Math.max(arr[0], arr[1]);
+		
+		for (int i = 2; i < n; i++) {
+			d[i] = Math.max(d[i-1], d[i-2]+arr[i]);
+		}
+		System.out.println(d[n-1]);
 	}
-	public static int[] d = new int[30001];
 	
 	//해당문제는 탑다운 방식으로 구현한 피보나치이다. 
 	public static void main(String[] args) {
-		//System.out.println(solution(26));
-		solution(26);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		solution(n, arr);
 	}	
 }
 	
